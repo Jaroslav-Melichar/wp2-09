8f300c8f8a10558340958e3c91f12a4287007a69
Jaroslav-Melichar <79139968+Jaroslav-Melichar@users.noreply.github.com>
2021-12-10 13:18:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,8 +11,21 @@
 
 <?php
 
+$pocetNakazenych = 5555;
 
 
+if($pocetNakazenych <= 10000){
+   echo "PES 1";
+}elseif(($pocetNakazenych >= 10001) && ($pocetNakazenych < 50000)){
+   echo "PES 2";
+}elseif(($pocetNakazenych >= 50001) && ($pocetNakazenych < 100000)){
+    echo "PES 3";
+}elseif(($pocetNakazenych >= 100001) && ($pocetNakazenych < 200000)){
+    echo "PES 4";
+}else {
+    echo "PES 5";
+}
+$return 
 ?>
 
 
